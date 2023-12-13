f52af1d65391995d30a2500bf2f90bd6ad77f14c
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2023-12-13 19:40:44 +0000

    final

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer (){
+    return (
+        <div className="d-flex justify-content-center bg-dark text-white">
+            <h4>Made with  <i className="fa-solid fa-heart"></i>  by Alex Fazakas</h4>
+        </div>
+    );
+};
+
+export default Footer;
